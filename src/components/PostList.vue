188ccfc3e3dc972282b343cd4f9119cce245fe97
Mariya Spiridonova <kolibri0509@gmail.com>
2023-11-15 18:13:11 +0300

    add PostList.vue

---
+++ src/components/PostList.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <div v-if="posts.length>0">
+            <h3 style="margin-top:15px">Список постов</h3>
+            <transition-group name="post-list">
+                <post-item v-for="post in posts" :key="post.id"
+                :post="post"
+                @remove="$emit('remove',post)"></post-item>
+            </transition-group>           
+        </div>
+        <h2 v-else style="color:red;margin-top:15px">Список постов пуст.</h2>
+    </div>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem.vue'
+export default {
+    props:{
+        posts:{
+           type:Array,
+           required:true,
+        }
+    },
+    components:{
+        PostItem,
+    },
+}
+</script>
+
+<style scoped>
+.post-list-item {
+  display: inline-block;
+  margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+  transition: all 0.5s ease;
+}
+.post-list-enter-from,
+.post-list-leave-to {
+  opacity: 0;
+  transform: translateX(130px);
+}
+.post-list-move{
+  transition: transform 0.8s ease;
+}
+</style>
