293bcab1e2f03cc5ce0d9042be0909cc7f486efa
Mariya Spiridonova <kolibri0509@gmail.com>
2023-11-15 16:00:05 +0300

    redaction main.js

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import directives from '@/directives'
 import store from './store'
 
 
-const app=createApp(App)
+const app = createApp(App)
 
 components.forEach(component=>{
     app.component(component.name,component)
